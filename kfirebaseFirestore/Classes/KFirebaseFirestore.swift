46f3ce7ee9f48c36a1e1a08d6a14d3846a3aefc7
Michelle Raouf <michelle.raouf@52ndsolution.net>
2024-10-13 16:47:06 +0300

    support multi listener and clean code

--- kfirebaseFirestore/Classes/KFirebaseFirestore.swift
+++ kfirebaseFirestore/Classes/KFirebaseFirestore.swift
@@ -1,13 +1,11 @@
 import FirebaseFirestore
 
-
-
 @objc public class KFirebaseFirestore: NSObject {
     private let firestore = Firestore.firestore()
     
-    private var listener: ListenerRegistration?
+    // Dictionary to hold multiple listeners
+    private var listeners: [String: ListenerRegistration] = [:]
 
-    
     @objc(addDocs:documentId:data:callback:)
     public func addDocs(
         collection: String,
@@ -25,7 +23,7 @@ import FirebaseFirestore
             }
         }
     }
-    
+
     @objc(getDocs:callback:)
     public func getDocs(
         collection: String,
@@ -36,12 +34,12 @@ import FirebaseFirestore
                 // Callback with error if there's an error
                 callback(KFirestoreListResult(data: nil, error: error))
             } else {
-                let data = querySnapshot?.documents.map { $0.data() } // Extract data from documents
-                callback(KFirestoreListResult(data: data, error: nil)) // Return data instead of documents
+                let data = querySnapshot?.documents.map { $0.data() }
+                callback(KFirestoreListResult(data: data, error: nil))
             }
         }
     }
-    
+
     @objc(getDocById:documentId:callback:)
     public func getDocById(
         collection: String,
@@ -52,12 +50,12 @@ import FirebaseFirestore
             if let error = error {
                 callback(KFirestoreResult(data: nil, error: error))
             } else {
-                let data = documentSnapshot?.data() // Extract data from document
-                callback(KFirestoreResult(data: data, error: nil)) // Return data instead of document
+                let data = documentSnapshot?.data()
+                callback(KFirestoreResult(data: data, error: nil))
             }
         }
     }
-    
+
     @objc(getDocsByFilter:orderBy:limit:filter:callback:)
     public func getDocsByFilter(
         collection: String,
@@ -67,7 +65,7 @@ import FirebaseFirestore
         callback: @escaping (KFirestoreListResult) -> Void
     ) {
         var query: Query = firestore.collection(collection)
-        
+
         for filter in filters {
             guard let filterPair = filter as? [String: Any],
                   let operatorStr = filterPair["operator"] as? String,
@@ -77,34 +75,15 @@ import FirebaseFirestore
             }
 
             let field = (filterPair["field"] as? String)?.split(separator: " ").first.map { String($0) } ?? ""
-            
-            // Log filter details
-            print("Applying filter: \(field) \(operatorStr) \(value)")
 
-            // Apply filter logic as before
             switch operatorStr {
-            case "==":
-                query = query.whereField(field, isEqualTo: value)
-            case "<":
-                if let numberValue = value as? NSNumber {
-                    query = query.whereField(field, isLessThan: numberValue)
-                }
-            case "<=":
-                if let numberValue = value as? NSNumber {
-                    query = query.whereField(field, isLessThanOrEqualTo: numberValue)
-                }
-            case ">":
-                if let numberValue = value as? NSNumber {
-                    query = query.whereField(field, isGreaterThan: numberValue)
-                }
-            case ">=":
-                if let numberValue = value as? NSNumber {
-                    query = query.whereField(field, isGreaterThanOrEqualTo: numberValue)
-                }
-            case "!=":
-                query = query.whereField(field, isNotEqualTo: value)
-            case "array-contains":
-                query = query.whereField(field, arrayContains: value)
+            case "==": query = query.whereField(field, isEqualTo: value)
+            case "<": query = query.whereField(field, isLessThan: value)
+            case "<=": query = query.whereField(field, isLessThanOrEqualTo: value)
+            case ">": query = query.whereField(field, isGreaterThan: value)
+            case ">=": query = query.whereField(field, isGreaterThanOrEqualTo: value)
+            case "!=": query = query.whereField(field, isNotEqualTo: value)
+            case "array-contains": query = query.whereField(field, arrayContains: value)
             case "array-contains-any":
                 if let arrayValue = value as? [Any] {
                     query = query.whereField(field, arrayContainsAny: arrayValue)
@@ -117,12 +96,9 @@ import FirebaseFirestore
                 if let arrayValue = value as? [Any] {
                     query = query.whereField(field, notIn: arrayValue)
                 }
-            default:
-                print("Unsupported operator: \(operatorStr)")
-                continue
+            default: print("Unsupported operator: \(operatorStr)")
             }
         }
-        
 
         if let orderBy = orderBy {
             query = query.order(by: orderBy as String)
@@ -144,83 +120,68 @@ import FirebaseFirestore
         }
     }
 
-
-
-
-
-    
     @objc(updateDocument:documentId:data:callback:)
     public func updateDocument(
         collection: String,
         documentId: String,
         data: [String: Any],
         callback: @escaping (KFirestoreResultUnit) -> Void
-    ){
-        
+    ) {
         firestore.collection(collection).document(documentId).updateData(data) { error in
             if let error = error {
-                // Callback with error if there's an error
                 callback(KFirestoreResultUnit(error: error))
             } else {
-                // Callback with no error
                 callback(KFirestoreResultUnit(error: nil))
             }
         }
     }
+
     @objc(deleteDoc:documentId:callback:)
     public func deleteDoc(
         collection: String,
         documentId: String,
         callback: @escaping (KFirestoreResultUnit) -> Void
-    ){
-        firestore.collection(collection).document(documentId).delete(){ error in
+    ) {
+        firestore.collection(collection).document(documentId).delete { error in
             if let error = error {
-                // Callback with error if there's an error
                 callback(KFirestoreResultUnit(error: error))
             } else {
-                // Callback with no error
                 callback(KFirestoreResultUnit(error: nil))
             }
-            
         }
-
     }
-   
+
     @objc(batchWriteDoc:updateOperations:deleteOperations:callback:)
     public func batchWriteDoc(
-        addOperations: [NSDictionary], // collection and data
-        updateOperations: [NSDictionary], // collection, documentId, data
-        deleteOperations: [NSDictionary], // collection and documentId
+        addOperations: [NSDictionary],
+        updateOperations: [NSDictionary],
+        deleteOperations: [NSDictionary],
         callback: @escaping (KFirestoreResultUnit) -> Void
     ) {
-        let batch = Firestore.firestore().batch()
+        let batch = firestore.batch()
 
-        // Add operations
         for operation in addOperations {
             guard let collection = operation["collection"] as? String,
                   let data = operation["data"] as? [String: Any] else { continue }
-            let documentRef = Firestore.firestore().collection(collection).document() // Auto ID
+            let documentRef = firestore.collection(collection).document()
             batch.setData(data, forDocument: documentRef)
         }
 
-        // Update operations
         for operation in updateOperations {
             guard let collection = operation["collection"] as? String,
                   let documentId = operation["documentId"] as? String,
                   let data = operation["data"] as? [String: Any] else { continue }
-            let documentRef = Firestore.firestore().collection(collection).document(documentId)
-            batch.setData(data, forDocument: documentRef)
+            let documentRef = firestore.collection(collection).document(documentId)
+            batch.updateData(data, forDocument: documentRef)
         }
 
-        // Delete operations
         for operation in deleteOperations {
             guard let collection = operation["collection"] as? String,
                   let documentId = operation["documentId"] as? String else { continue }
-            let documentRef = Firestore.firestore().collection(collection).document(documentId)
+            let documentRef = firestore.collection(collection).document(documentId)
             batch.deleteDocument(documentRef)
         }
 
-        // Commit the batch
         batch.commit { error in
             if let error = error {
                 callback(KFirestoreResultUnit(error: error))
@@ -229,28 +190,43 @@ import FirebaseFirestore
             }
         }
     }
-    
-    @objc(startRealTimeListener:callback:)
-       public func startRealTimeListener(
-           collection: String,
-           callback: @escaping (KFirestoreListResult) -> Void
-       ) {
-           listener = firestore.collection(collection)
-               .addSnapshotListener { (querySnapshot, error) in
-                   if let error = error {
-                       // Callback with error if there's an error
-                       callback(KFirestoreListResult(data: nil, error: error))
-                   } else {
-                       let data = querySnapshot?.documents.map { $0.data() }
-                       callback(KFirestoreListResult(data: data, error: nil)) // Return the data
-                   }
-               }
-       }
 
-       // Stop listener method
-       @objc(stopRealTimeListener)
-       public func stopRealTimeListener() {
-           listener?.remove()
-           listener = nil
-       }
+    // Start a real-time listener and store it with a unique key
+    @objc(startRealTimeListener:listenerId:callback:)
+    public func startRealTimeListener(
+        collection: String,
+        listenerId: String,
+        callback: @escaping (KFirestoreListResult) -> Void
+    ) {
+        let listener = firestore.collection(collection)
+            .addSnapshotListener { querySnapshot, error in
+                if let error = error {
+                    callback(KFirestoreListResult(data: nil, error: error))
+                } else {
+                    let data = querySnapshot?.documents.map { $0.data() }
+                    callback(KFirestoreListResult(data: data, error: nil))
+                }
+            }
+        
+        // Store the listener with the provided listenerId
+        listeners[listenerId] = listener
+    }
+
+    // Stop a specific listener by listenerId
+    @objc(stopRealTimeListenerById:)
+    public func stopRealTimeListenerById(listenerId: String) {
+        if let listener = listeners[listenerId] {
+            listener.remove()
+            listeners.removeValue(forKey: listenerId)
+        }
+    }
+
+    // Stop all listeners
+    @objc(stopAllListeners)
+    public func stopAllListeners() {
+        for (_, listener) in listeners {
+            listener.remove()
+        }
+        listeners.removeAll()
+    }
 }
